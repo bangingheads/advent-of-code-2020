1c5e7d5b67b6c6c4d70e4787bbccb4b1033b17db
bangingheads <53184036+bangingheads@users.noreply.github.com>
2020-12-03 00:25:18 -0600

    Squashed commit of the following:
    commit bd7fe39131c1ea43b2aa3e9001feb578b90978bc
    Author: bangingheads <53184036+bangingheads@users.noreply.github.com>
    Date:   Thu Dec 3 00:16:36 2020 -0600
    
        Day 3 cleanup

--- days/day3.py
+++ days/day3.py
@@ -2,30 +2,25 @@ data = open("day3.txt").read().splitlines()
 
 
 def part_one():
-    y = 0
-    coordinate = 0
-    trees = 0
+    y, trees = 0, 0
+    line_length = len(data[0])
     for x in range(len(data)):
-        if data[x][coordinate] == "#":
-            trees += 1
+        trees += data[x][y % line_length] == "#"
         y += 3
-        coordinate = y % 31
     return trees
 
 
 def part_two():
     trees = [0, 0, 0, 0, 0]
     lines = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    line_length = len(data[0])
     for i, (left, right) in enumerate(lines):
         y = 0
-        coordinate = 0
         for x in range(len(data)):
             if x % right != 0:
                 continue
-            if data[x][coordinate] == "#":
-                trees[i] += 1
+            trees[i] += data[x][y % line_length] == "#"
             y += left
-            coordinate = y % 31
     total = 1
     for x in trees:
         total *= x
